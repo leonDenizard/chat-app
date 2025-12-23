fd8c7a057543698e3d13faa0e91ad83bbcce68f1
leonDenizard <de.nizard@hotmail.com>
2025-12-23 19:25:05 -0300

    style: updated text-color searchbar

--- src/components/header-aside/HeaderAside.tsx
+++ src/components/header-aside/HeaderAside.tsx
@@ -14,7 +14,7 @@ export default function HeaderAside({setIsAsideOpen}: HeaderAsideProps){
             <Search className="absolute left-3 top-1/2 -translate-y-1/2 w-4 h-4 text-zinc-900 dark:text-zinc-200" />
             <Input
               placeholder="Search chats..."
-              className="cursor-not-allowed pl-10 bg-violet-200 dark:bg-zinc-700 border-none h-10 text-zinc-900 dark:text-white dark:placeholder:text-zinc-200"
+              className="cursor-not-allowed pl-10 bg-violet-200 dark:bg-zinc-700 border-none h-10 text-zinc-900 dark:text-white dark:placeholder:text-zinc-400"
             />
             <X className="md:hidden relative cursor-pointer text-zinc-700 dark:text-zinc-200 hover:dark:text-zinc-100 hover:bg-zinc-300 dark:hover:bg-zinc-700 rounded-lg transition-colors duration-300 h-10 w-12 p-2"
               onClick={() => setIsAsideOpen(false)}
